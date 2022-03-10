32a09260938e0d21d8fda5bb74005074619fa34b
Vikash Patel <pvickey663@gmail.com>
2022-03-10 10:53:17 +0530

    commited full

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+//security
+
+const helmet= require('helmet')
+const cors=require('cors')
+const xss= require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
+
 //connnect DB
 const connectDB=require('./db/connect')
 
@@ -16,8 +24,18 @@ const pathRouter=require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy',1)
+
+app.use(rateLimiter({
+  windowsMs : 15*60*1000,
+  max:100,
+}
+))
 
 app.use(express.json());
+app.use(helmet())
+app.use(cors())
+app.use(xss())
 
 
 // routes
